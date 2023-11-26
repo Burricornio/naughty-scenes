5a393646b94ba689c6003b5747d01a9bdd3b1417
Burricornio <alejandro_web@hotmail.com>
2023-11-26 14:48:48 +0100

    Create scenes number input

---
+++ src/components/SceneMiniaturesComponent.vue
@@ -0,0 +1,87 @@
+<template>
+  <ul class="scene-miniatures-container">
+    <li
+      v-for="scene in scenes"
+      :key="scene.id"
+      @click="showSceneInfoModal(scene)"
+      class="scene-miniature"
+      :class="[
+        {
+          'current-scene': currentScene && scene.id === currentScene!.id,
+          'played-scene': checkIfUsedScene(scene.id)
+        }
+      ]"
+    >
+      {{ scene.id }}
+    </li>
+  </ul>
+  <ModalComponent :open="open" @close-modal="closeModal">
+    <div class="miniature-modal" v-if="clickedScene">
+      <h2>{{ clickedScene.title }}</h2>
+      <p>{{ clickedScene.instructions }}</p>
+    </div>
+  </ModalComponent>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Scene } from '@/stores/useSceneStore'
+import ModalComponent from '@/components/ModalComponent.vue'
+import useModal from '@/composables/useModal'
+
+const props = defineProps<{
+  scenes: Scene[]
+  playedSceneIds: number[]
+  currentScene: Scene | null
+}>()
+
+const { open, openModal, closeModal } = useModal()
+
+const clickedScene = ref<Scene | null>(null)
+
+// METHODS
+function checkIfUsedScene(id: number): boolean {
+  return props.playedSceneIds.includes(id)
+}
+
+function showSceneInfoModal(scene: Scene) {
+  clickedScene.value = scene
+  openModal()
+}
+</script>
+
+<style lang="scss" scoped>
+.scene-miniatures-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  list-style-type: none;
+  background: lightpink;
+  margin-top: 20px;
+  padding: 0;
+  cursor: pointer;
+
+  .scene-miniature {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 80px;
+    height: 80px;
+    background: lightcoral;
+    margin: 10px;
+  }
+
+  .current-scene {
+    background: red;
+  }
+
+  .played-scene {
+    position: relative;
+    background: green;
+  }
+}
+
+.miniature-modal {
+  margin: 20px;
+}
+</style>
---
+++ src/components/SelectScenesNumberInputComponent.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="select-scenes-number">
+    <div class="input-container">
+      <label>{{ text.scenesNumberLabel }}:</label>
+      <input
+        type="number"
+        v-bind="scenesNumberInput"
+        :max="sceneStore.getDefaultScenesNumberLength"
+        :min="minNumber"
+      />
+      <p class="errors">{{ errors.scenesNumber }}</p>
+    </div>
+    <button :disabled="disabledBtn" @click="selectScenes">
+      {{ text.selectScenesNumber }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useForm } from 'vee-validate'
+import { useI18n } from 'vue-i18n'
+import * as yup from 'yup'
+import { useSceneStore } from '@/stores/useSceneStore'
+
+// STORE
+const sceneStore = useSceneStore()
+
+const minNumber = 1
+
+// TEXTS
+const { t } = useI18n()
+const text = {
+  scenesNumberLabel: t('view.director.scenes_number_label'),
+  maxRule: t('rule.max', { number: sceneStore.getScenesLength }),
+  minRule: t('rule.min', { number: minNumber }),
+  requiredRule: t('rule.required'),
+  selectScenesNumber: t('button.select')
+}
+
+// DATA
+const scenesNumber = ref<number>(3)
+
+const { errors, defineInputBinds } = useForm({
+  initialValues: {
+    scenesNumber: scenesNumber.value
+  },
+  validationSchema: yup.object({
+    scenesNumber: addRules()
+  })
+})
+const scenesNumberInput = defineInputBinds('scenesNumber')
+
+// COMPUTED
+const disabledBtn = computed(() => {
+  return scenesNumberInput.value.value
+    ? scenesNumberInput.value.value > sceneStore.getDefaultScenesNumberLength
+    : true
+})
+
+// METHODS
+function selectScenes(): void {
+  if (
+    scenesNumberInput.value.value! <= sceneStore.getDefaultScenesNumberLength
+  ) {
+    sceneStore.selectRandomScenes(scenesNumberInput.value.value as number)
+  }
+}
+
+function addRules() {
+  return yup
+    .number()
+    .transform((val, orig) => (orig === '' ? undefined : val))
+    .required(text.requiredRule)
+    .min(minNumber, text.minRule)
+    .max(sceneStore.getDefaultScenesNumberLength, text.maxRule)
+}
+</script>
+
+<style lang="scss" scoped>
+.select-scenes-number {
+  @include flex($align-items: flex-start);
+
+  .input-container {
+    margin-bottom: 10px;
+
+    input {
+      width: 200px;
+    }
+  }
+
+  button {
+    min-width: 130px;
+    margin: 22px 0 0 10px;
+  }
+}
+</style>
