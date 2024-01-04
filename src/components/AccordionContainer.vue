35e922e0b64677aaec56b4327429d813009e0c6f
Burricornio <alejandro_web@hotmail.com>
2024-01-04 12:01:31 +0100

    Create ACCORDION component

---
+++ src/components/AccordionContainer.vue
@@ -0,0 +1,68 @@
+<template>
+  <draggable
+    class="accordion-container"
+    v-model="scenes"
+    item-key="id"
+    group="scenes"
+    :animation="200"
+  >
+    <template #item="{ element }">
+      <div class="acordeon-item">
+        <header @click="toggleAccordion(element)">
+          <h2>Vamos: {{ element.isOpenAccordion }}</h2>
+          <h2>{{ element.id }}</h2>
+        </header>
+        <section v-if="element.isOpenAccordion">
+          <div>{{ element.instructions }}</div>
+        </section>
+      </div>
+    </template>
+  </draggable>
+</template>
+
+<script setup lang="ts">
+import { AccordionScene } from '@/stores/useSceneStore'
+import { computed } from 'vue'
+import draggable from 'vuedraggable'
+
+const props = defineProps<{
+  scenes: AccordionScene[]
+}>()
+
+const scenes = computed(() => props.scenes)
+
+function toggleAccordion(scene: AccordionScene) {
+  scene.isOpenAccordion = !scene.isOpenAccordion
+}
+</script>
+
+<style lang="scss" scoped>
+.accordion-container {
+  @include flex($flex-direction: column);
+  margin-bottom: 20px;
+
+  .acordeon-item {
+    margin: 10px;
+    width: 400px;
+
+    header {
+      cursor: pointer;
+      background-color: teal;
+      padding: 10px;
+
+      h2 {
+        margin: 0;
+        color: white;
+      }
+    }
+
+    section {
+      @include flex($flex-direction: column);
+      background-color: green;
+      width: 100%;
+      height: 50px;
+      color: white;
+    }
+  }
+}
+</style>
--- src/views/DirectorView.vue
+++ src/views/DirectorView.vue
@@ -6,28 +6,16 @@
       @change-scenes-number-length="selectRandomScenes"
     />
     <div v-if="scenes.length">
-      <draggable v-model="scenes" item-key="id" group="scenes" :animation="200">
-        <template #item="{ element }">
-          <div class="acordeon-item">
-            <header @click="toggleAcordeon(element)">
-              <h2>Vamos: {{ element.isOpenAccordion }}</h2>
-              <h2>{{ element.id }}</h2>
-            </header>
-            <section v-if="element.isOpenAccordion">
-              <div>Holasassaasass</div>
-            </section>
-          </div>
-        </template>
-      </draggable>
+      <AccordionContainerComponent :scenes="scenes" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import AccordionContainerComponent from '@/components/AccordionContainer.vue'
 import SelectScenesNumberInputComponent from '@/components/SelectScenesNumberInputComponent.vue'
 import { AccordionScene, useSceneStore } from '@/stores/useSceneStore'
-import draggable from 'vuedraggable'
 
 // STORE
 const sceneStore = useSceneStore()
@@ -47,38 +35,11 @@ function selectRandomScenes(numberOfScenes: number): void {
   sceneStore.selectRandomScenes(numberOfScenes)
   scenes.value = sceneStore.getScenes as AccordionScene[]
 }
-
-function toggleAcordeon(scene: AccordionScene) {
-  console.log('opemnnnnnnn', scene)
-  scene.isOpenAccordion = !scene.isOpenAccordion
-  console.log(scene)
-}
 </script>
 
 <style lang="scss" scoped>
 .director-view {
   @include flex($flex-direction: column);
   margin-bottom: 20px;
-
-  .acordeon-item {
-    header {
-      cursor: pointer;
-      background-color: teal;
-      padding: 10px;
-      h2 {
-        margin: 0;
-        color: white;
-      }
-    }
-
-    section {
-      @include flex($flex-direction: column);
-      background-color: green;
-      width: 400px;
-      margin: 20px;
-      height: 50px;
-      color: white;
-    }
-  }
 }
 </style>
