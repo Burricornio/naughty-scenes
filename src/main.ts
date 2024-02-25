93ee7d9cdb3c65aaabe051d831f7e0898eadca9f
Burricornio <alejandro_web@hotmail.com>
2024-02-25 14:24:12 +0100

    Add pinia-plugin-persistedstate plugin

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 // https://yesicon.app/
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from '@/router'
 import i18n from './plugins/vue-i18n'
 import './styles/global.scss'
 import App from './App.vue'
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 
 app.use(router)
